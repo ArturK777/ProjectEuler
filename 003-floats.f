13f7d414c134275560c97b2d0daffea726fae9d1
ArturK777 <ArturK777@users.noreply.github.com>
2021-01-16 17:21:35 +0300

    Largest prime factor
    The prime factors of 13195 are 5, 7, 13 and 29.
    What is the largest prime factor of the number 600851475143 ?

---
+++ 003-floats.f
@@ -0,0 +1,19 @@
+REQUIRE FLOATS lib/include/float.f
+FINIT \ initialize FPU
+600851475143. 2CONSTANT N
+: F>S  ( f -- u )   F>D D>S ;
+: PRIME2?  ( u -- flag )   \ is 5 <= u (odd) a prime number - short ver
+   DUP 3 DO DUP I MOD IF ELSE DROP 0 LEAVE THEN 2 +LOOP ;
+: BRUT  ( start -- u )   DUP 2 MOD 0= IF 1+ THEN
+   BEGIN
+      2- DUP PRIME2? 
+      IF N 2 PICK UM/MOD DROP 0= ELSE 0 THEN
+   UNTIL ;
+N D>F FSQRT F>S BRUT .
+
+\EOF 
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+Простые множители 13195: 5, 7, 13 и 29.
+Какой самый большой простой делитель числа 600 851 475 143?
