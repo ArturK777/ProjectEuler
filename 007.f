7c73efb1a5eec7e8672effe2a65c0f6a60b2ddfb
ArturK777 <ArturK777@users.noreply.github.com>
2021-01-16 17:45:21 +0300

    10001st prime
    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
    What is the 10 001st prime number?
    104743

---
+++ 007.f
@@ -0,0 +1,22 @@
+: PRIME?  ( u -- ? )   \ is u a prime number
+   DUP 3 DO DUP I MOD IF ELSE DROP 0 LEAVE THEN
+   2 +LOOP ; \ odd only
+
+: PRIME-COUNT  ( i -- n )   1- 1 3 
+   BEGIN
+      -ROT 2DUP >
+	WHILE 
+      ROT 2+ DUP PRIME? 
+	IF SWAP 1+ SWAP THEN
+   REPEAT 2DROP ;
+10001 PRIME-COUNT .
+
+\EOF 104743
+Problem 7
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
+we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+
+ѕеречислив первые шесть простых чисел: 2, 3, 5, 7, 11 и 13,
+мы видим, что шестое простое число равно 13.
+ акое 10 001-е простое число?
