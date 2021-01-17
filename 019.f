bae895cf4d569787260915a5751bd6ec694357ed
ArturK777 <ArturK777@users.noreply.github.com>
2021-01-17 12:00:54 +0300

    Counting Sundays
    You are given the following information, but you may prefer to do some research for yourself.
        1 Jan 1900 was a Monday.
        Thirty days has September,
        April, June and November.
        All the rest have thirty-one,
        Saving February alone,
        Which has twenty-eight, rain or shine.
        And on leap years, twenty-nine.
        A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
    How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
    171

---
+++ 019.f
@@ -0,0 +1,36 @@
+12 2001 1901 - * 7 / .
+
+\EOF
+
+2001 \ end
+1901 - \ begin
+12 * \ months
+7 / \ days in week
+.
+
+
+
+\EOF 171
+Counting Sundays
+Problem 19
+You are given the following information, but you may prefer to do some research for yourself.
+    1 Jan 1900 was a Monday.
+    Thirty days has September,
+    April, June and November.
+    All the rest have thirty-one,
+    Saving February alone,
+    Which has twenty-eight, rain or shine.
+    And on leap years, twenty-nine.
+    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+Вам предоставляется следующая информация, но вы можете предпочесть провести небольшое исследование самостоятельно.
+    1 января 1900 года был понедельник.
+    Тридцать дней есть сентябрь,
+    Апрель, июнь и ноябрь.
+    У всех остальных тридцать один,
+    Спасая только февраль,
+    Которая имеет двадцать восемь, в дождь или в ясную погоду.
+    А в високосных - двадцать девять.
+    Високосный год наступает в любой год, делимый на 4, но не в столетие, если он не делится на 400.
+Сколько воскресений приходилось на первое число месяца в двадцатом веке (с 1 января 1901 года по 31 декабря 2000 года)?
