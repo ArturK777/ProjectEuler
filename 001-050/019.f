80edce2536a883ff9c5d64e4013049d5de4ed6df
ArturK777 <ArturK777@users.noreply.github.com>
2021-01-17 15:28:40 +0300

    Rename 019.f to 001-050/019.f

---
+++ 001-050/019.f
@@ -0,0 +1,36 @@
+12 2001 1901 - * 7 / .
+
+\EOF
+
+2001 \ end
+1901 - \ begin
+12 * \ months
+7 / \ days in week
+.
+
+
+
+\EOF 171
+Counting Sundays
+Problem 19
+You are given the following information, but you may prefer to do some research for yourself.
+    1 Jan 1900 was a Monday.
+    Thirty days has September,
+    April, June and November.
+    All the rest have thirty-one,
+    Saving February alone,
+    Which has twenty-eight, rain or shine.
+    And on leap years, twenty-nine.
+    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+Âàì ïðåäîñòàâëÿåòñÿ ñëåäóþùàÿ èíôîðìàöèÿ, íî âû ìîæåòå ïðåäïî÷åñòü ïðîâåñòè íåáîëüøîå èññëåäîâàíèå ñàìîñòîÿòåëüíî.
+    1 ÿíâàðÿ 1900 ãîäà áûë ïîíåäåëüíèê.
+    Òðèäöàòü äíåé åñòü ñåíòÿáðü,
+    Àïðåëü, èþíü è íîÿáðü.
+    Ó âñåõ îñòàëüíûõ òðèäöàòü îäèí,
+    Ñïàñàÿ òîëüêî ôåâðàëü,
+    Êîòîðàÿ èìååò äâàäöàòü âîñåìü, â äîæäü èëè â ÿñíóþ ïîãîäó.
+    À â âèñîêîñíûõ - äâàäöàòü äåâÿòü.
+    Âèñîêîñíûé ãîä íàñòóïàåò â ëþáîé ãîä, äåëèìûé íà 4, íî íå â ñòîëåòèå, åñëè îí íå äåëèòñÿ íà 400.
+Ñêîëüêî âîñêðåñåíèé ïðèõîäèëîñü íà ïåðâîå ÷èñëî ìåñÿöà â äâàäöàòîì âåêå (ñ 1 ÿíâàðÿ 1901 ãîäà ïî 31 äåêàáðÿ 2000 ãîäà)?
