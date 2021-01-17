fdd72a4c46d70b41eecfcd414c80fc27f9a5dc9c
ArturK777 <ArturK777@users.noreply.github.com>
2021-01-17 13:45:20 +0300

    Champernowne's constant
    An irrational decimal fraction is created by concatenating the positive integers:
    0.123456789101112131415161718192021...
    It can be seen that the 12th digit of the fractional part is 1.
    If dn represents the nth digit of the fractional part, find the value of the following expression.
    d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
    210

---
+++ 040.f
@@ -0,0 +1,27 @@
+: FILL#  ( -- adr )
+   1000010 ALLOCATE THROW DUP 1000000 + OVER  1 
+   BEGIN DUP S>D <# #S #>  DUP 2>R 2 PICK R>  CMOVE R> ROT + SWAP 1+
+      OVER 3 PICK >
+   UNTIL  DROP 2DROP  ;
+: PEEK*  ( adr -- mul ) 1 1
+   7 0 DO 0 2OVER + 1- C@ 48 -
+      NIP * SWAP 10 * SWAP
+   LOOP -ROT 2DROP ;
+FILL#  PEEK* . 
+
+\EOF 210
+Champernowne's constant
+Problem 40
+An irrational decimal fraction is created by concatenating the positive integers:
+0.123456789101112131415161718192021...
+It can be seen that the 12th digit of the fractional part is 1.
+If dn represents the nth digit of the fractional part, find the value of the following expression.
+d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
+
+Постоянная Чамперноуна
+Проблема 40
+Иррациональная десятичная дробь создается путем объединения положительных целых чисел:
+0,123456789101112131415161718192021 ...
+Видно, что 12-я цифра дробной части равна 1.
+Если dn представляет собой n-ю цифру дробной части, найдите значение следующего выражения.
+d1 * d10 * d100 * d1000 * d10000 * d100000 * d1000000
